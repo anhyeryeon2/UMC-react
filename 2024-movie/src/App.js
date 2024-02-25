6d04d7bc214737576a6e97804d19130d2cd3de9d
rony <ahr020532@gmail.com>
2024-02-25 22:47:34 +0900

    movie page

---
+++ 2024-movie/src/App.js
@@ -0,0 +1,16 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+
+function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
+    </HashRouter>
+  );
+}
+
+export default App;
