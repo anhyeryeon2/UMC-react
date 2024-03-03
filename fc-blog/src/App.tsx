b9730ea620ede8ef5420a6a002b8bc398e3bf824
rony <ahr020532@gmail.com>
2024-03-03 23:42:08 +0900

    components

---
+++ fc-blog/src/App.tsx
@@ -0,0 +1,13 @@
+
+import { Route, Routes, Navigate,Link} from 'react-router-dom';
+import Router from './components/Router';
+
+function App() {
+  return (
+    <>
+     <Router/>
+    </>
+  );
+}
+
+export default App;
