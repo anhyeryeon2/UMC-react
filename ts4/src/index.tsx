8e033c7da555bde29b7609ccc2f7dbe3bf206859
rony <ahr020532@gmail.com>
2024-03-10 01:11:52 +0900

    3/10

---
+++ ts4/src/index.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+import React from "react";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
